f535bcaa2bf30a2d0a4f7b436c26634ea2355573
Casey MacLeod <caseymacleod22@gmail.com>
2021-06-22 13:20:15 -0400

    create react app, set initial state

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {useState, useEffect} from 'react'
+import './App.css';
+import reactDom from 'react-dom';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;

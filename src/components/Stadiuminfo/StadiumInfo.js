415c99464c8f67b2a3dedbd64945aafcd42c74c7
Casey MacLeod <caseymacleod22@gmail.com>
2021-07-02 12:40:32 -0400

    adds StadiumInfo component

---
+++ src/components/Stadiuminfo/StadiumInfo.js
@@ -0,0 +1,12 @@
+import {Link} from 'react-router-dom'
+
+const StadiumInfo = (props) => {
+    return (
+        <div>
+            <h1>Stadium Info</h1>
+            <Link to='/'>Home Page</Link>
+        </div>
+    )
+}
+
+export default StadiumInfo
